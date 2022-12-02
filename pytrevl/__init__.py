c1563cdeecb9c3cea7b38b1f59398ac89781df50
Valentin Lorenzen <valentinlorenzen@mailbox.org>
2022-12-02 16:14:44 +0100

    Shortened importing scheme for package modules

---
+++ pytrevl/__init__.py
@@ -0,0 +1,12 @@
+'''
+Import modules to shorten import path to use i.e.:
+
+pytrevl.Dashboard()
+
+instead of
+
+pytrevl.dashboard.Dashboard()
+'''
+
+from .dashboard import Dashboard
+from .components import PieChart, BarChart
