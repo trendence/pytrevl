55f5ed19afee836a58b9607c6b6401da6e48dda2
Valentin Lorenzen <valentinlorenzen@mailbox.org>
2022-12-06 20:41:48 +0100

    added get_trevl function to Chart parent class

--- pytrevl/chart_parent_class.py
+++ pytrevl/chart_parent_class.py
@@ -1,4 +1,5 @@
 from highcharts import Highchart
+import yaml
 
 from pytrevl.api.x_middle import X_Middle
 from pytrevl.trevl_code_generator import Trevl_Code_Generator
@@ -12,10 +13,16 @@ class Chart:
         trevl_code = trevl_code_generator.create_trevl()
         return trevl_code
         
-    def get_yaml(self) -> str:
+    def get_yaml(self) -> dict:
         trevl_code_generator = Trevl_Code_Generator(self)
         yaml_code = trevl_code_generator.create_yaml()
         return yaml_code
+    
+    def get_trevl(self) -> str:
+        trevl_code_generator = Trevl_Code_Generator(self)
+        yaml_code = trevl_code_generator.create_yaml()
+        trevl_code = yaml.safe_dump(yaml_code)
+        print(trevl_code)
 
     def show(self) -> Highchart:
         x_middle_api = X_Middle(self)
