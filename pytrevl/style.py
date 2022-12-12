c27be8298bca5118b0b82aec14fa23d19ffee14c
Valentin Lorenzen <valentinlorenzen@mailbox.org>
2022-12-12 12:49:55 +0100

    fixed donut_chart rendering

--- pytrevl/style.py
+++ pytrevl/style.py
@@ -1,6 +1,5 @@
 class Style:
 
-    def __init__(self, title: str, innerSize: str = None, size: int = 100):
+    def __init__(self, title: str, innerSize: str = None):
         self.title = title
         self.innerSize = innerSize
-        self.size = size
