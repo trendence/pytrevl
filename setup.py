46dbc2d9e3b162064006ec6f5106993a07813886
Valentin Lorenzen <valentinlorenzen@mailbox.org>
2022-12-14 15:47:40 +0100

    fixed setup.py bug

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pytrevl',
-    version='0.2.0',    
+    version='0.2.1',    
     description='Python interface to handle and modify TREVL backed visualizations',
     url='https://github.com/trendence/pytrevl',
     author='Trendence Institut GmbH',
